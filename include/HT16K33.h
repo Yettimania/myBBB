826e15f50d08f5fde688fccf2454ec9688639590
Yettimania <kylesnyder42@gmail.com>
2020-07-03 08:52:25 -0700

    Began HT16K33 library.

--- include/HT16K33.h
+++ include/HT16K33.h
@@ -1,6 +1,10 @@
 #ifdef HT16K33
 #define HT16K33
 
-int initialize(int);
+int startOscillation(void);
+//int setDisplay(char);
+int setBrightness(int);
+int blink(int);
+//int blank(void);
 
 #endif

ec7737aece0e69afbad073f78334be0a19d1f3b4
Yettimania <kylesnyder42@gmail.com>
2020-07-03 09:54:30 -0700

    Library.

---
+++ libHT16K33/HT16K33.c
@@ -0,0 +1,52 @@
+/* These are the functions associated with the HT16K33 chip to control the 7 segment display over i2c. These functions are based on the datasheet and are the primary functions to use the device.
+Author: Kyle Snyder
+Date: July 03, 2020
+*/
+
+/* These are the used registers for the 7 segment display
+that need to be accessed to write the valid characters.*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
+#include <HT16K33.h>
+
+static char usedReg[4] = { 0x00, 0x02, 0x06, 0x08 };
+
+int
+startOscillation(int file){
+
+   char cmd[1] = {0x21};
+
+   if (write(file, cmd, 1) != 1){
+       perror("failed to start oscillator\n");
+       return 1;
+   }
+}
+
+int
+setBrightness(int file, int x){
+
+   char cmd[1] = {0xef};
+
+   if (x<0 || x>15) {
+       perror("brightness setting between 0 and 15\n");
+       return 1;
+   } else {
+       if (write(file, cmd, 1) != 1) {
+           perror("Failed to set brightness\n");
+           return 1;
+       }
+   }
+}
+
+int
+blink(int file, int x){
+   char cmd[1] = {0x81};
+   if (write(file, cmd, 1) != 1){
+       perror("Failed to write blink register\n");
+       return 1;
+   }
+}
